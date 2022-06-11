654c9d89845fa344ed24849cab3f6a754bcc2488
Xiehua <xh125@mail.ustc.edu.cn>
2022-06-11 18:11:41 +0800

    add script in epw calculation

--- tools/epw-sub/runepw.sh
+++ tools/epw-sub/runepw.sh
@@ -10,9 +10,9 @@ for i in $(seq 6 6 36)
   	sed -i "s:elecselfen:elecselfen=.false. !:g" epw${i}.in
   
 	sed -i "s:nkf1:nkf1=$i !:g" epw${i}.in
-	sed -i "s:nkf1:nkf2=$i !:g" epw${i}.in  
+	sed -i "s:nkf2:nkf2=$i !:g" epw${i}.in  
 	sed -i "s:nqf1:nqf1=$i !:g" epw${i}.in
-	sed -i "s:nqf2:nqf1=$i !:g" epw${i}.in  
+	sed -i "s:nqf2:nqf2=$i !:g" epw${i}.in  
 
 	cp qe-epw.bsub qe-epw${i}.bsub
 	sed -i "2s:epw:epw${i}:g" qe-epw${i}.bsub
@@ -20,5 +20,4 @@ for i in $(seq 6 6 36)
 	sed -i "s:epw.out:epw${i}.out:g" qe-epw${i}.bsub	
 
 	bsub < qe-epw${i}.bsub
-  	wait 10
 	done
